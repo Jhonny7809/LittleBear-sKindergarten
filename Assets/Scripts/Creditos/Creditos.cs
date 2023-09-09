c36a8273ec6a7e9c8f3f1ff8cab5cc830da2c9d5
Vicio <cdmx2418@amerike.edu.mx>
2023-09-09 00:30:13 -0600

    Creditos.
    Se agrego la escena Creditos y las funciones para regresar al menu.

---
+++ Assets/Scripts/Creditos/Creditos.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Creditos : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("WaitforEnd", 30);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("MenuIncial");
+        }
+
+        if (Input.GetKey(KeyCode.F))
+        {
+            SceneManager.LoadScene("MenuInicial");
+        }
+
+
+    }
+
+    public void WaitforEnd()
+    {
+        SceneManager.LoadScene("MenuInicial");
+    }
+}
