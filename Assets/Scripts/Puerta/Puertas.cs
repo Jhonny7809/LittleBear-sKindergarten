951b2f00871b9f864d0dbf258f6c9a4beff6ccd0
Jonathan Montesinos <jonathan_2910MP@outlook.com>
2023-09-09 20:56:08 -0600

    Puertas ya funcionales

--- Assets/Scripts/Puerta/Puertas.cs
+++ Assets/Scripts/Puerta/Puertas.cs
@@ -4,51 +4,16 @@ using UnityEngine;
 
 public class Puertas : MonoBehaviour
 {
-    public float speed;
-    public float angle;
-    public Vector3 direction;
+    public Animator anim;
 
-    public bool puedeAbrir;
-    public bool abrir;
-
-    void Start()
+    public void OnTriggerStay(Collider collision)
     {
-        angle = transform.eulerAngles.y;
-    }
-
-    void Update()
-    {
-        if (Mathf.Round(transform.eulerAngles.y) != angle)
+        if (collision.gameObject.tag == "Player")
         {
-            transform.Rotate(direction * speed);
-        }
-        if (Input.GetButtonDown("Fire1") && puedeAbrir == true && abrir == false)
-        {
-            angle = 80;
-            direction = Vector3.up;
-            abrir = true;
-        }
-        else if (Input.GetButtonDown("Fire1") && puedeAbrir == true && abrir == true)
-        {
-            angle = -80;
-            direction = Vector3.down;
-            abrir = false;
-        }
-    }
-
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.gameObject.tag == "Player")
-        {
-            puedeAbrir = true;
-        }
-    }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.tag == "Player")
-        {
-            puedeAbrir = false;
+            if (Input.GetButtonDown("Fire1"))
+            {
+                anim.SetBool(("Abrir"),true);
+            }
         }
     }
 }
