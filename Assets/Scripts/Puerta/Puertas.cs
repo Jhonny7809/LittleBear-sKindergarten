f119d882b3053f57410f775e1dc3ef7a0e0755dd
Jonathan Montesinos <jonathan_2910MP@outlook.com>
2023-09-10 16:00:06 -0600

    sonidos y cambios

--- Assets/Scripts/Puerta/Puertas.cs
+++ Assets/Scripts/Puerta/Puertas.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class Puertas : MonoBehaviour
 {
-    public AudioSource sonido;
+    public AudioSource sonido1;
+    public AudioSource sonido2;
     public Animator anim;
-    public GameObject coll;
 
     public void OnTriggerStay(Collider collision)
     {
@@ -15,18 +15,8 @@ public class Puertas : MonoBehaviour
             if (Input.GetButtonDown("Fire1"))
             {
                 anim.SetBool(("Abrir"), true);
-            }
-        }
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "Player")
-        {
-            if (other.tag == "Player" && !sonido.isPlaying)
-            {
-                sonido.Play();
-                coll.SetActive(false);
+                sonido1.Pause();
+                sonido2.Play();
             }
         }
     }
