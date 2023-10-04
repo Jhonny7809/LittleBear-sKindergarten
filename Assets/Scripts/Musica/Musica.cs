227ca3524500d5111f41e7c8fb537ef5b9347e96
Viciouz Sholensk <cdmx2418@amerike.edu.mx>
2023-10-04 01:12:03 -0600

    CAmbios en musica de fondo
    agregue un efecto de volumen

--- Assets/Scripts/Musica/Musica.cs
+++ Assets/Scripts/Musica/Musica.cs
@@ -1,17 +1,51 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEngine;
 
 public class Musica : MonoBehaviour
 {
-    private AudioSource msrc;
-
-    private void start()
+    private AudioSource _msrc;
+    void Start()
     {
-        msrc = GetComponent<AudioSource>();
-        msrc.volume = 0f;
+        _msrc = GetComponent<AudioSource>();
+        _msrc.volume = 0f;
+        StartCoroutine(Fade(true, _msrc, 2f, 0.21f));
+        StartCoroutine(Fade(false, _msrc, 2f, 0f));
     }
 
-   //public IEnumerator Fade(bool fadeIn, AudioSource msrc)
-    
+    private void Update()
+    {
+        if (!_msrc.isPlaying)
+        {
+            _msrc.Play();
+            StartCoroutine(Fade(true, _msrc, 2f, 0.21f));
+            StartCoroutine(Fade(false, _msrc, 2f, 0f));
+        }
+    }
+
+    // Update is called once per frame
+    public IEnumerator Fade(bool fadeIn, AudioSource src, float duration, float targetVol)
+    {
+        if (!fadeIn)
+        {
+            double lenthOfSouce = (double)src.clip.samples / _msrc.clip.frequency;
+            yield return new WaitForSecondsRealtime((float)(lenthOfSouce-duration));
+        }
+
+        float time = 0f;
+        float startVol = src.volume;
+        while (time < duration)
+        {
+            string fadeSituation = fadeIn ? "fadeIn" : "fadeOut";
+            Debug.Log(fadeSituation);
+            time += Time.deltaTime*0.1f;
+            src.volume = Mathf.Lerp(startVol, targetVol, time / duration);
+            yield return null;
+        }
+
+        yield break;
+    }
 }
